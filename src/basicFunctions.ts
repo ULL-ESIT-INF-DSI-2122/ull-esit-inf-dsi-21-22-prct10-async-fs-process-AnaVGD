c6a40ad08a309e6a04aff0caf5bb02fb9fb7063d
AnaVGD <alu0101322650@ull.edu.es>
2022-04-26 21:44:32 +0000

    pruebas de codigo

--- src/basicFunctions.ts
+++ src/basicFunctions.ts
@@ -1,11 +1,31 @@
-import * as express from 'express';
+/**
+ * Adds two numbers
+ * @param firstNumber Consists of the first numeric operand of the addition
+ * @param secondNumber Consists of the second numeric operand of the addition
+ * @return The addition of the two numbers `firstNumber` and `secondNumber`
+ *
+ * Usage:
+ * ```typescript
+ * add(1, 7) = 8
+ * add(1.7, 3.5) = 5.2
+ * ```
+ */
+export function add(firstNumber: number, secondNumber: number) {
+  return firstNumber + secondNumber;
+}
 
-const app = express();
-
-app.get('', (_, res) => {
-  res.send('Hello World!');
-});
-
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
-});
+/**
+ * Substracts two numbers
+ * @param firstNumber Consists of the first numeric operand of the substraction
+ * @param secondNumber Consists of the second numeric operand of the substraction
+ * @return The substraction of the two numbers `firstNumber` and `secondNumber`
+ *
+ * Usage:
+ * ```typescript
+ * substract(7, 1) = 6
+ * substract(8.6, 3.3) = 5.3
+ * ```
+ */
+export function substract(firstNumber: number, secondNumber: number) {
+  return firstNumber - secondNumber;
+}
